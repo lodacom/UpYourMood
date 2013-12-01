f4401b7ceba51f8f00d93f2383f79c170e0dc02b
Pierre <pierre@pierre-K53SD.(none)>
2013-12-01 01:38:18 +0100

    JAVADOC NiceTag

--- app/models/NiceTag.java
+++ app/models/NiceTag.java
@@ -4,9 +4,19 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 
+/**
+ * Classe implémentant les informations relatives à l'ontologie NiceTag.
+ * @author BURC Pierre, DUPLOUY Olivier, KISIALIOVA Katsiaryna, SEGUIN Tristan
+ *
+ */
+
 public class NiceTag {
 	public static final String nicetag="http://ns.inria.fr/nicetag/2010/09/09/voc.html#";
 	
+	/**
+	 * Méthode simple jouant le rôle de "getter".
+	 * @return l'uri de nicetag.
+	 */
 	public static String getURI(){
 		return nicetag;
 	}
